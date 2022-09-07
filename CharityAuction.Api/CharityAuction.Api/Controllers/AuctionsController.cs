1465ce11ff847b1d3fd6659f0a95f1eec956dc20
Andrew <andrew@synapscommerce.com>
2022-09-08 00:44:25 +1200

    added controller

---
+++ CharityAuction.Api/CharityAuction.Api/Controllers/AuctionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CharityAuction.Api.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
